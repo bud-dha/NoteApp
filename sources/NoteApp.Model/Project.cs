4659b6ff45f4cd7081d1910fd07e67564552a7b5
Nikita Martsenkovsky <martsenkovskynikita@icloud.com>
2022-05-26 14:10:20 +0700

    features: add sort

--- sources/NoteApp.Model/Project.cs
+++ sources/NoteApp.Model/Project.cs
@@ -15,5 +15,32 @@ namespace NoteApp.Model
         /// Возвращает и задает список заметок пользователя.
         /// </list>
         public List<Note> Notes { get; set; } = new List<Note>();
+        /// <summary>
+        /// Текущая заметка.
+        /// </summary>
+        public Note CurrentNote { get; set; }
+
+        /// <summary>
+        /// Возвращает список заметок отсортированных по дате редактирования.
+        /// </summary>
+        public List<Note> NotesByDate()
+        {
+            if (Notes.Count != 0)
+            {
+                Notes.OrderBy(t => t.ModifiedDateTime);
+            }
+            return Notes;
+        }
+        /// <summary>
+        /// Возвращает список заметок отсортированных по катрегории.
+        /// </summary>
+        public List<Note> NotesByCategory(NoteCategory category)
+        {
+            if (Notes.Count != 0)
+            {
+                Notes.Where(t => t.Category == category);
+            }
+            return Notes;
+        }
     }
 }
